17aefd5d8eb7cbadfaed9f3904306299eee8b4be
Jonah Murray <jonahmurray321@gmail.com>
2020-06-18 14:36:31 -0500

    added /random path

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
 
     componentDidMount() {
-        fetch("https://vpce-0bd02e6e0e09e7544-4e9yt2ud-us-east-1d.execute-api.us-east-1.vpce.amazonaws.com", {
+        fetch("https://vpce-0bd02e6e0e09e7544-4e9yt2ud-us-east-1d.execute-api.us-east-1.vpce.amazonaws.com/random/", {
             method: 'GET',
             headers: {
             'x-api-key': '7xggh6vvja',
