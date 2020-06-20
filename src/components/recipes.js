140256663fa9b554b0f85e9a62a3c33f22e36d8f
Jonah Murray <jonahmurray321@gmail.com>
2020-06-20 09:12:38 -0500

    added 2 ingredients

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -7,8 +7,10 @@ const Recipes = ({recipes}) => {
             { recipes.map((recipe) => (
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">{recipe.strDrink}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{recipe.strCategory}, ID: {recipe.idDrink}</h6>
+                        <h3 class="card-title">{recipe.strDrink}</h3>
+                        <h4 class="card-subtitle mb-2 text-muted">{recipe.strCategory}, ID: {recipe.idDrink}</h4>
+                        <h5 class="card-subtitle mb-2 text-muted">Ingedient1: {recipe.strIngredient1} -- {recipe.strMeasure1} </h5>
+                        <h5 class="card-subtitle mb-2 text-muted">Ingedient2: {recipe.strIngredient2} -- {recipe.strMeasure2} </h5>
                         <p class="card-text">{recipe.strInstructions}</p>
                     </div>
                 </div>
